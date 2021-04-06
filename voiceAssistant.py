612b81c1e83813c4607f14c398d3ae031e64446a
Rohit Shailesh <76203282+RohitShailesh@users.noreply.github.com>
2021-04-06 18:56:19 +0530

    Update voiceAssistant.py
    opening  google ,YouTube.
    searching info using Wikipedia

--- voiceAssistant.py
+++ voiceAssistant.py
@@ -1,15 +1,20 @@
 import pyttsx3
 import datetime
-import speech_recognition as sr
 import pywhatkit
+import wikipedia
+from GoogleNews import GoogleNews
+import webbrowser
+import speech_recognition as sr
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-print(voices[0].id)
+
 engine.setProperty('voice',voices[0].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
 
 def wishMe():
     hour=float(datetime.datetime.now().hour)
@@ -35,7 +40,7 @@ def takeCommand():
     try:
         print("recognizing...")
         query= r.recognize_google(audio,language='en-in')
-        print("user said:",query)
+        print(query)
 
     except  Exception as e:
         print(e)
@@ -50,11 +55,36 @@ def runPy():
     print(command)
     if 'play' in command:
         song=command.replace('play','')
+        speak("playing")
+        print('playing')
         pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time=datetime.datetime.now().strftime('%I:%M %p')
+        speak("current time is"+time)
+        print(time)
+    elif 'Wikipedia' in command:
+        key=command.replace("wikipedia","")
+        info=wikipedia.summary(key)
+        print(info)
+        speak(info)
+    elif 'news' in command:
+        news=GoogleNews(lang='en')
+        speak(news)
+    elif 'open YouTube' in command:
+        webbrowser.open("youtube.com")
+        speak("openning")
+    elif 'open Google' in command:
+        webbrowser.open("google.com")
+        speak("openning")
+
+
+
+
+runPy()
+
+
 
         
 takeCommand()
 
 
-
-    
